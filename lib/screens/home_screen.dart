fd21a04aef8559d02143a79975b8f4e1f65abfce
sunbang123 <musicianksy@naver.com>
2024-08-10 11:42:47 +0900

    movie poster ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,14 +28,13 @@ class HomeScreen extends StatelessWidget {
         future: movies,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var movie = snapshot.data![index];
-                return Text(movie.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
             );
           }
           return const Center(
@@ -45,132 +44,44 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  ListView makeList(AsyncSnapshot<List<MovieModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var movie = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3),
+                  )
+                ],
+              ),
+              child: Image.network(movie.fullPosterPath),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              movie.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
+    );
+  }
 }
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:toonflix/models/movie_model.dart';
-// import 'package:toonflix/services/api_service.dart';
-
-// class HomeScreen extends StatelessWidget {
-//   HomeScreen({super.key, required List<MovieModel> movies});
-
-//   Future<List<MovieModel>> movies = ApiService.getPopularMovies();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: AppBar(
-//         elevation: 2,
-//         backgroundColor: Colors.white,
-//         foregroundColor: Colors.green,
-//         title: const Center(
-//           child: Text(
-//             "오늘의 영화",
-//             style: TextStyle(
-//               fontSize: 24,
-//             ),
-//           ),
-//         ),
-//       ),
-//       body: FutureBuilder(
-//         future: movies,
-//         builder: (context, snapshot) {
-//           if (snapshot.hasData) {
-//             return const Text("There is data!");
-//           }
-//           return const Text('Loading....');
-//         },
-//       ),
-//     );
-//   }
-// }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:toonflix/models/movie_model.dart';
-// import 'package:toonflix/services/api_service.dart';
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key, required List<MovieModel> movies});
-
-//   @override
-//   State<HomeScreen> createState() => _HomeScreenState();
-// }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   List<MovieModel> movies = [];
-//   bool isLoading = true;
-
-//   void waitForMovies() async {
-//     movies = await ApiService.getPopularMovies();
-//     isLoading = false;
-//     setState(() {});
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     waitForMovies();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print(movies);
-//     print(isLoading);
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: AppBar(
-//         elevation: 2,
-//         backgroundColor: Colors.white,
-//         foregroundColor: Colors.green,
-//         title: const Center(
-//           child: Text(
-//             "오늘의 영화",
-//             style: TextStyle(
-//               fontSize: 24,
-//             ),
-//           ),
-//         ),
-//       ),
-//       body: const Center(
-//         child: Text("Home Screen Content"),
-//       ),
-//     );
-//   }
-// }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:toonflix/models/movie_model.dart';
-
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({super.key, required List<MovieModel> movies});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: AppBar(
-//         elevation: 2,
-//         backgroundColor: Colors.white,
-//         foregroundColor: Colors.green,
-//         title: const Center(
-//           child: Text(
-//             "오늘의 영화",
-//             style: TextStyle(
-//               fontSize: 24,
-//             ),
-//           ),
-//         ),
-//       ),
-//       body: const Center(
-//         child: Text("Home Screen Content"),
-//       ),
-//     );
-//   }
-// }
